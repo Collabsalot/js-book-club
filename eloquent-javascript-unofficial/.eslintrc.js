5741beab18303bdb9063ddbf471fda36e2924fb5
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-17 13:00:54 -0600

    Configure ESLint

---
+++ eloquent-javascript-unofficial/.eslintrc.js
@@ -0,0 +1,22 @@
+const ERROR = 'error'
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+  rules: {
+    'comma-dangle': [ERROR, 'always-multiline']
+  },
+}
