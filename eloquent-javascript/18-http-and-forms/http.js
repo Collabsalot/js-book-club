da4e4029770eb4945a7edcfaca88fadd631f4cd4
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-17 03:25:00 -0600

    Add code files (#3)

---
+++ eloquent-javascript/18-http-and-forms/http.js
@@ -0,0 +1,25 @@
+function backgroundReadFile(url, callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.addEventListener("load", function() {
+    if (req.status < 400)
+      callback(req.responseText);
+  });
+  req.send(null);
+}
+
+function getURL(url, callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.addEventListener("load", function() {
+    if (req.status < 400)
+      callback(req.responseText);
+    else
+      callback(null, new Error("Request failed: " +
+        req.statusText));
+  });
+  req.addEventListener("error", function() {
+    callback(null, new Error("Network error"));
+  });
+  req.send(null);
+}
